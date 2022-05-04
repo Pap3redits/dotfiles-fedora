ba77188f45d4ff5cb630b5f0814efc628cb82ca2
Christian <pap3redits@gmail.com>
2022-05-04 12:41:25 -0400

    added rpm fusion to the install script

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ cp -r .config ~/.config
 
 sudo chmod +x ~/.config/awesome/noobie/filesystem.sh ~/.config/awesome/noobie/memory.sh
 
+sudo dnf install \
+	  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install \
+	  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 sudo dnf install $(cat packages|xargs)
 

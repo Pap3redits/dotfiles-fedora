af7eeb464eb5f197b802a7c413b82ca441577c68
Christian <pap3redits@gmail.com>
2022-05-04 12:28:00 -0400

    added the packages file

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-cp .config ~/.config
+cp -r .config ~/.config
 
 sudo chmod +x ~/.config/awesome/noobie/filesystem.sh ~/.config/awesome/noobie/memory.sh
 
+sudo dnf install $(cat packages|xargs)
 
